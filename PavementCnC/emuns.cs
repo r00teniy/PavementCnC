9cbcb5698a21abe71132e67e8ea2985a2ca60787
r00teniy <r00teniy@gmail.com>
2023-01-01 11:14:44 +0500

    feat: added enums

---
+++ PavementCnC/emuns.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace PavementCnC
+{
+    enum CurbType
+    {
+        Granite,
+        Concrete,
+        Metall,
+        Plastic
+    }
+    enum PavementType
+    {
+        Asphalt,
+        Concrete,
+        Tiles,
+        LooseFill,
+        Rubber,
+        Grass
+    }
+    enum GreeneryType
+    {
+        Tree,
+        Shrub,
+        FlowerBed,
+        Hedge,
+        Planter
+    }
+    enum StreetFurtitureType
+    {
+        Child,
+        Sport,
+        Trash,
+        Rest,
+        RoadSign
+    }
+}
